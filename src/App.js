d4293902464d9bdb929a3d86b66e02f04231b4ac
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-09-27 20:16:43 -0700

    Troubleshoot errors preventing project from building

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my app
         </p>
         <a
           className="App-link"
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as firebase from "firebase/app";
-// import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
