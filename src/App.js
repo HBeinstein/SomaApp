f4ea48fa498a0d6650cc96ae80f87e155a194e18
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-09-28 20:42:25 -0700

    Connect firebase realtime db to firebase.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import database from './firebase';
+
+function writeUserData(userId, name, email) {
+  database.ref('users/' + userId).set({
+    username: name,
+    email: email,
+  });
+}
 
 function App() {
+
+
+  console.log(database);
+  // writeUserData('000', 'Hannah', 'meow@gmail.com');
+
+  // let database = firebase.database();
+
+  // var ref = database.ref('somawebapp');
+
+  // ref.on('value', dataReceived, errData)
+
+  // function dataReceived(data) {
+  //   console.log(data);
+  // }
+
+  // function errData(err){
+  //   console.log("err");
+  //   console.log(error);
+  // }
 
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -3,19 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID 
-}
-
-firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
