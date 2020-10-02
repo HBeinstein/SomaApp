574e7de0b3d0f740d6a0d89e79947196a97a28d2
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-10-02 10:04:13 -0700

    Update routes in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,17 +12,6 @@ export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/mobile">Mobile</Link>
-            </li>
-          </ul>
-        </nav>
-
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
