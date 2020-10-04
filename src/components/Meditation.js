450a69116e31859892a28b75a5bc0575a2dd8b1f
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-10-03 19:40:44 -0700

    Make last commit before integrating threejs

--- src/components/Meditation.js
+++ src/components/Meditation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { database } from '../firebase';
 // require('firebase/database');
+import './../assets/css/animation.css'
 
 function Meditation() {
   // READ data from db, retrieves once
@@ -27,6 +28,7 @@ return (
   <React.Fragment>
     <h3>Meditation Page</h3>
     <button onClick={getData}>Start Meditation</button>
+    <div className='animation-test'></div>
   </React.Fragment>
 );
 }
