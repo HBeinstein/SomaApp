76f643f4f284097fd5cc52a663cce7574375e6ba
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-10-05 11:42:54 -0700

    Continue troubleshooting how to animate imported model in scripts component

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,5 +1,3 @@
-/* ------------------- OG PLZ SAVE ---------------------*/
-
 import React, { useRef, useEffect, Component } from 'react';
 import * as THREE from 'three';
 import { AmbientLight } from 'three';
@@ -7,108 +5,17 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import '../assets/css/animation.css';
 // import OrbitControls from "three-orbitcontrols";
 
-// //Canvas Size
-
-// const canvasSize = {
-//   height: '100vh',
-//   width: '100vw',
-//   backgroundColor: 'blue'
-// }
-
-// function Scene() {
-//   const threeScene = useRef(null);
-//   let renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-//   let scene = new THREE.Scene();
-//   let camera = new THREE.PerspectiveCamera(
-//     45, // vertical field of view
-//     100/100, // aspect ratio
-//     1, //near plane
-//     1000 // 1000 //far plane
-//   );
-//   const renderScene = () => {
-//     renderer.render(scene, camera)
-//   }
-  
-//   useEffect(() => {
-//     let width = threeScene.current.clientWidth;
-//     let height = threeScene.current.clientHeight;
-
-//     let loader = new GLTFLoader;
-
-//     loader.load('./models/tRex/scene.gltf', gltf => {
-//       console.log(gltf.scene);
-//       scene.add(gltf.scene);
-//     });
-
-//     const resizeScene = () => {
-//       width = threeScene.current.clientWidth;
-//       height = threeScene.current.clientHeight;
-//       renderer.setSize(width, height);
-//       camera.aspect = width / height;
-//       camera.updateProjectionMatrix();
-//       // renderer.render( scene, camera );
-//       renderScene();
-//       // console.log(scene);
-//       // window.removeEventListener('resize', resizeScene);
-//     }
-
-//     camera.position.set(0, 1, 10);
-//     const ambient = new THREE.AmbientLight(0X404040, 10);
-//     scene.add(ambient);
-//     renderer.setSize(width, height);
-//     // renderer.setPixelRatio(window.devicePixelRatio);
-//     // camera.aspect = width / height;
-
-//     threeScene.current.appendChild(renderer.domElement);
-//     console.log('sup', renderer.domElement);
-//     window.addEventListener('resize', resizeScene);
-//     // renderer.render( scene, camera );
-
-//     return () => {
-//       window.removeEventListener('resize', resizeScene);
-//       threeScene.current.removeChild(renderer.domElement);
-      
-//     }
-    
-
-//     // stop()
-//   }, []);
-    
-//   console.log('jhglhjgkjfhdd')
-
-    
-//   return(
-//     <React.Fragment>
-//       { renderScene() }
-//       <div className="threeScene" style={canvasSize} ref={threeScene}>
-//       </div>
-//     </React.Fragment>
-//   );
-// }
-
-// export default Scene;
-
-/* ------------------- OG PLZ SAVE ---------------------*/
-
-/* ----------------------------------------*/
-/* ----------------------------------------*/
-/* ----------------------------------------*/
-/* ----------------------------------------*/
-/* ----------------------------------------*/
-
-/* ------------------- NEW STUFF ---------------------*/
-
 function Scene(props) {
   const { useRef, useEffect, useState } = React
   const mount = useRef(null)
   const [isAnimating, handleAnimation] = useState(true)
   const controls = useRef(null)
+  let tRex = null;
   
   useEffect(() => {
     let width = mount.current.clientWidth;
     let height = mount.current.clientHeight;
     let activelyAnimating;
-    let tRex = null;
 
     const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
@@ -117,11 +24,10 @@ function Scene(props) {
     let loader = new GLTFLoader;
 
     loader.load('./models/tRex/scene.gltf', gltf => {
-      scene.add(gltf.scene);
-      tRex = gltf.scene.children[1];
-      console.log(`trex ${tRex}`);
+      tRex = gltf;
     });
 
+    console.log(tRex);
     //Add light to scene
     const ambient = new THREE.AmbientLight(0X404040, 10);
     scene.add(ambient);
@@ -175,6 +81,7 @@ function Scene(props) {
     //Append scene to DOM, start animation
     mount.current.appendChild(renderer.domElement);
     window.addEventListener('resize', handleResize);
+    console.log(scene);
     startAnimation();
 
     controls.current = { startAnimation, endAnimation };
@@ -183,7 +90,6 @@ function Scene(props) {
       endAnimation();
       window.removeEventListener('resize', handleResize);
       mount.current.removeChild(renderer.domElement);
-
       scene.remove(tRex);
     }
   }, [])
