c1510484411a050942ce342db8952f56ea59ab9e
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-10-06 17:49:23 -0700

    Add orbit control and position to model in scene component

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 import { AmbientLight } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import '../assets/css/animation.css';
-// import OrbitControls from "three-orbitcontrols";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 function Scene(props) {
   const { useRef, useEffect, useState } = React
@@ -19,30 +19,31 @@ function Scene(props) {
 
     const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
-    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+    const renderer = new THREE.WebGLRenderer({ antialias: true })
     
 
     let loader = new GLTFLoader;
 
     loader.load('./models/soma1.gltf', gltf => {
+      gltf.scene.position.set(0, -10, 10);
       scene.add(gltf.scene);
       base = gltf;
     });
 
     console.log(base);
     //Add light to scene
-    const ambient = new THREE.AmbientLight(0X404040, -30);
+    const ambient = new THREE.AmbientLight(0X404040, 10);
     scene.add(ambient);
 
     // Set camera position
-    camera.position.z = 5;
+    camera.position.z = 25;
     // camera.position.x = 20;
-    camera.position.y = 1;
-    camera.lookAt(10, 5, 0);
-
+    camera.position.y = 5;
+    // camera.lookAt(10, 5, 0);
 
     //Set clear background color in conjunction with alpha:true in renderer & renderer size
-    renderer.setClearColor( 0xffffff, 0);
+    scene.background = new THREE.Color(0x00FFFF);
+    // renderer.setClearColor( 0xffffff, 0);
     renderer.setSize(width, height);
 
     //Handle re-render when window is resized (triggered via event listener)
@@ -55,6 +56,9 @@ function Scene(props) {
       animate()
     }
     
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.update();
+
     //Define animation actions in here-- will loop and handle animation
     function animate (value) {
       value *= 0.001;  // convert to seconds --> eventually put dynamic value here
@@ -64,7 +68,8 @@ function Scene(props) {
       }
 
       renderer.render(scene, camera);
-      requestAnimationFrame(animate)
+      requestAnimationFrame(animate);
+      controls.update();
     }
 
     console.log(base);
