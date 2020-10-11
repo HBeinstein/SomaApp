8827cffc31ed4d06cc51019c57938daf915618b1
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-10-10 21:41:35 -0700

    Add styling to home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import '../assets/css/index.css';
+import '../assets/css/navbar.css';
+import '../assets/css/app.css';
 import Home from './Home';
 import About from './About';
 import MeditationControl from './MeditationControl';
@@ -7,28 +10,41 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  // Link
+  Link
 } from "react-router-dom";
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/authorize">
-            <AuthorizationControl />
-          </Route>
-          <Route path="/meditations">
-            <MeditationControl />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <React.Fragment>
+      <Router>
+        <div className="navbar-full-container">
+        <Link to="/"><h1>Soma</h1></Link>
+
+          <div className="navbar-links-container">
+            <Link to="/"><h5>Home</h5></Link>
+            <Link to="/about"><h5>About</h5></Link>
+            <Link to="/meditations"><h5>Meditations</h5></Link>
+            <Link to="/authorize"><h5>Authorize</h5></Link>
+          </div>
+        </div>
+
+        <div className="app-content-container">
+          <Switch>
+            <Route path="/authorize">
+              <AuthorizationControl />
+            </Route>
+            <Route path="/meditations">
+              <MeditationControl />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </React.Fragment>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import '../assets/css/index.css';
+import '../assets/css/home.css';
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          SOMA Meditation 
-        </p>
-      </header>
+    <div className="home-container">
+      <div className="home-body-flex-container">
+        <div className="home-body-text">
+          <h2>
+            Empowing people to embrace mindfullness in their everyday routines.
+          </h2>
+        </div>
+
+        <div className="home-img">
+
+        </div>
+      </div>
+
+      <div className="home-why-meditation">
+        <h3>Why meditation?</h3>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <a href="/meditations"><button>Try it out!</button></a>
+      </div>
+
+      <div className="home-how-it-works">
+        <h3>How it works</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nam libero justo laoreet sit amet cursus. Egestas sed sed risus pretium quam vulputate dignissim suspendisse in. Nulla aliquet porttitor lacus luctus accumsan tortor. Vestibulum lectus mauris ultrices eros in. Duis ut diam quam nulla porttitor.</p>
+      </div>
+
+      <div className="home-daily-routine">
+        <h3>Bring mindfullness into your daily routine</h3>
+        <p>Vitae semper quis lectus nulla. Purus sit amet luctus venenatis. Eget gravida cum sociis natoque penatibus et magnis dis. Ante metus dictum at tempor commodo. Cursus metus aliquam eleifend mi in. Id semper risus in hendrerit gravida rutrum quisque non tellus. Ante in nibh mauris cursus. Morbi tristique senectus et netus et malesuada fames. A diam sollicitudin tempor id eu. Odio facilisis mauris sit amet.</p>
+      </div>
     </div> 
   );
 }
